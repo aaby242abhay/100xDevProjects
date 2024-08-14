114f65907883f0dedf53c189c0bd7516abb87e13
Aaby Abhay <aaby242abhay@gmail.com>
2024-08-14 18:21:49 +0530

    nextjs backend

--- week13/my-app/app/page.tsx
+++ week13/my-app/app/page.tsx
@@ -1,14 +1,24 @@
 import axios from "axios";
-
 async function getUserDetails() {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 5*1000);
+  })
   const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
 	return response.data;
 }
-
-export default function Home() {
+export default async function Home() {
+  const userDetails = await getUserDetails();       //this is a server component and data fetching_call is being done on the server
   return (
-    <div>
-      Hello World
+    <div className="flex flex-col justify-center h-screen">
+    <div className="flex justify-center">
+        <div className="border p-8 rounded">
+            <div>
+                Name: {userDetails?.name}
+            </div>
+            {userDetails?.email}
+        </div>
     </div>
+</div>
   );
 }
+
